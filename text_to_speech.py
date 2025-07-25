b70a164a95f823343af8b97f28634c7a46be6cbe
Krishna Rajoria <rajoriakrishna0@gmail.com>
2025-07-25 12:31:18 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,8 +1,9 @@
 import os
-import elevenlabs
-from elevenlabs.client import ElevenLabs
+#import elevenlabs
+#from elevenlabs.client import ElevenLabs
 import subprocess
 import platform
+from gtts import gTTS
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,7 +11,7 @@ load_dotenv()
 ELEVENLABS_API_KEY=os.environ.get("ELEVENLABS_API_KEY")
 
 
-def text_to_speech_with_elevenlabs(input_text, output_filepath):
+'''def text_to_speech_with_elevenlabs(input_text, output_filepath):
     client=ElevenLabs(api_key=ELEVENLABS_API_KEY)
     audio=client.text_to_speech.convert(
         text= input_text,
@@ -30,6 +31,32 @@ def text_to_speech_with_elevenlabs(input_text, output_filepath):
         else:
             raise OSError("Unsupported operating system")
     except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")'''
+
+
+from gtts import gTTS
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{output_filepath}").PlaySync();'])
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
         print(f"An error occurred while trying to play the audio: {e}")
+        
         
 
